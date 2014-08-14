6ee315487df743a74ac6ec5f8f42e27b7aa92ea4
Stefan Baumgartner <sbaumg@gmail.com>
2014-08-15 00:00:21 +0200

    major update, now it looks really usable

---
+++ app/scripts/main.js
@@ -0,0 +1,24 @@
+var lss = CodeMirror(document.getElementById('less'), {
+	mode: "text/x-less",
+	theme: "monokai",
+	tabSize: 2,
+	indentWithTabs: false,
+	//lineNumbers: true
+});
+
+
+var css = CodeMirror(document.getElementById('css'), {
+	mode: "text/css",
+	theme: "monokai",
+	tabSize: 2,
+	indentWithTabs: false,
+	//lineNumbers: true
+});
+
+var parser = new less.Parser();
+
+lss.on('change', function() {
+	parser.parse(lss.doc.getValue(), function (e, tree) {
+			css.setValue(tree.toCSS());
+	});
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-var gulp = require('gulp'),
-  less   = require('gulp-less'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  usemin = require('gulp-usemin');
+var gulp 	= require('gulp'),
+  less   	= require('gulp-less'),
+  concat 	= require('gulp-concat'),
+  uglify 	= require('gulp-uglify'),
+  connect 	= require('gulp-connect'),
+  usemin 	= require('gulp-usemin');
 
 gulp.task('less', function() {
 	return gulp.src('app/styles/main.less')
 		.pipe(less())
+		.pipe(connect.reload())
 		.pipe(gulp.dest('dist/styles'));
 });
 
@@ -14,6 +16,13 @@ gulp.task('watch', function() {
 	gulp.watch('app/styles/**/*.less', ['less']);
 });
 
+gulp.task('connect', function() {
+	connect.server({
+		root: ['app','dist'],
+		livereload: true
+	});
+});
+
 gulp.task('default', ['less']);
 
-gulp.task('dev', ['watch']);
+gulp.task('dev', ['connect', 'watch']);
